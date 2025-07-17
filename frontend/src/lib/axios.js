09b0c0f3277d805627ed6779917c87b538805dbd
Kalyan Krapa <krapa.kalyan@gmail.com>
2025-07-17 18:58:05 +0530

    KD

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000/api" // your local backend
-    : "https://your-backend-service.onrender.com/api"; // your deployed Render backend
+    : "https://vibezee.onrender.com/api"; // your deployed Render backend
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
