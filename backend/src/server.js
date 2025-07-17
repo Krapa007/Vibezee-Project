8563135888bf9d3eb4b4ae43fe40b87c1124583a
Kalyan Krapa <krapa.kalyan@gmail.com>
2025-07-17 10:00:20 +0530

    Done

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,7 +32,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173" || "",
     credentials: true, // allow frontend to send cookies
   })
 );
