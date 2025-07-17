94fc4e3a3b69c847ce946a1928c336bd2ae3e961
Kalyan Krapa <krapa.kalyan@gmail.com>
2025-07-17 10:06:30 +0530

    Done

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,7 +32,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "http://localhost:5173" || "",
+    origin: ["http://localhost:5173","https://vibezee-frontend.vercel.app/logins" ],
     credentials: true, // allow frontend to send cookies
   })
 );
