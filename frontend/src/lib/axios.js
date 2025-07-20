18871c4371a6113eb6d7ee2e64d3474030e880f7
Kalyan Krapa <krapa.kalyan@gmail.com>
2025-07-20 20:53:34 +0530

    Hi

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,23 @@
+// import axios from "axios";
+
+// const BASE_URL =
+//   import.meta.env.MODE === "development"
+//     ? "https://vibezee-backend.onrender.com/api"
+//     : "/api";
+
+// export const axiosInstance = axios.create({
+//   baseURL: BASE_URL,
+//   withCredentials: true, //sends cookies with the request
+// });
+
 import axios from "axios";
 
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "https://vibezee-backend.onrender.com/api"
-    : "/api";
+    ? "http://localhost:3000/api"
+    : "https://vibezee-backend.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, //sends cookies with the request
+  withCredentials: true,
 });
