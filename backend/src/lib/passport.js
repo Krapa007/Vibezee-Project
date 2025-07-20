f92ab6370053e51b1a947e440da19b0ec3e34c83
Kalyan Krapa <krapa.kalyan@gmail.com>
2025-07-20 15:48:01 +0530

    Hiii

--- backend/src/lib/passport.js
+++ backend/src/lib/passport.js
@@ -9,7 +9,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/api/auth/google/callback",
+      callbackURL:
+        "https://vibezee-backend.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
