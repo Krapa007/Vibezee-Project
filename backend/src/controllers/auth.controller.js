4b20e2e1bfb524788a06329c9071189f02697d50
Kalyan Krapa <krapa.kalyan@gmail.com>
2025-07-20 22:18:43 +0530

    Bye

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -124,7 +124,9 @@ export async function login(req, res) {
 }
 
 export function logout(req, res) {
-  res.clearCookie("jwt");
+  res.clearCookie("jwt", {
+    path: "/",
+  });
   res.status(200).json({ success: true, message: "Logout successful" });
 }
 
